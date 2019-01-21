f71650f87189c978770cd5958b93bb84a6f1a711
modmuss50 <modmuss50@gmail.com>
2019-01-21 18:16:16 +0000

    Remove debug

--- lua/dectector.lua
+++ lua/dectector.lua
@@ -30,27 +30,15 @@ local function dectectorMain()
     local data = json.decodeFromFile(dataFile)
 
     while true do
-        local var1, var2, minecartType, minecartName, primaryColor, secondaryColor, destination, ownerName = os.pullEvent("minecart")
+        local event, color, minecartType, minecartName, destination, ownerName = os.pullEvent("minecart")
         local passData = {
             info = data,
             minecart = {
                 type = minecartType,
                 name = minecartName,
-                color = primaryColor,
-                color2 = secondaryColor,
                 dest = destination,
                 owner = ownerName
             }
-        }
-        passData = {
-            v1 = var1,
-            v2 = var2,
-            v3 = minecartType,
-            v4 = minecartName,
-            v5 = primaryColor,
-            v6 = secondaryColor,
-            v7 = destination,
-            v8 = ownerName
         }
         utils.httpPostAsync("detector/pass", passData)
     end
