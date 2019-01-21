03f179c36d16e3101a248d58913be5591fc548a6
modmuss50 <modmuss50@gmail.com>
2019-01-21 16:59:54 +0000

    get around caching in api

--- lua/utils.lua
+++ lua/utils.lua
@@ -2,15 +2,21 @@ os.loadAPI("json.lua")
 
 local apiURL = "http://playground.gaz492.uk:8000/"
 
+--Gets around caching
+function getUrlSuffix()
+    return ("?t=" .. math.random(1, 1000000))
+end
+
 function get(path)
-    local get = http.get(path)
+    local get = http.get(apiURL .. path .. getUrlSuffix())
     local response  = json.decode(get.readAll())
     return response
 end
 
 function post(path, data)
     local body = json.encode(data)
-    local post = http.post(apiURL .. path, body)
+    local post = http.post(apiURL .. path .. getUrlSuffix(), body)
     local response  = json.decode(post.readAll())
     return response
 end
+
