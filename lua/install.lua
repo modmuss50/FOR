16e2e58b1987d8d90e9e07be0a0863389fed79cc
modmuss50 <modmuss50@gmail.com>
2019-01-21 16:44:01 +0000

    fixes?

--- lua/install.lua
+++ lua/install.lua
@@ -1,34 +1,38 @@
-local baseURL = "https://raw.githubusercontent.com/modmuss50/FOR/master/lua/"
+installFOR()
 
-print("Downloading file list")
+function installFOR()
+    local baseURL = "https://raw.githubusercontent.com/modmuss50/FOR/master/lua/"
 
-local response = http.get(baseURL .. "files.txt".. getUrlSuffix())
+    print("Downloading file list")
 
-local body = response.readAll()
-local lines = {}
+    local response = http.get(baseURL .. "files.txt" .. getUrlSuffix())
 
-for s in body:gmatch("[^\r\n]+") do
-    table.insert(lines, s)
-end
+    local body = response.readAll()
+    local lines = {}
 
-for i = 1, #lines do
-    local filename = lines[i]
-    print("Downloading: " .. filename)
-    if fs.exists(filename) then
-        print("Deleting old " .. filename)
-        fs.delete(filename)
+    for s in body:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
     end
 
-    local url = baseURL .. filename .. getUrlSuffix()
-    local fileBody = http.get(url).readAll()
-    local file = assert(fs.open(filename, "w"))
-    file.write(fileBody)
-    file.close()
-end
+    for i = 1, #lines do
+        local filename = lines[i]
+        print("Downloading: " .. filename)
+        if fs.exists(filename) then
+            print("Deleting old " .. filename)
+            fs.delete(filename)
+        end
 
-print("Done, downloaded " .. table.getn(lines) .. " files")
+        local url = baseURL .. filename .. getUrlSuffix()
+        local fileBody = http.get(url).readAll()
+        local file = assert(fs.open(filename, "w"))
+        file.write(fileBody)
+        file.close()
+    end
+
+    print("Done, downloaded " .. table.getn(lines) .. " files")
+end
 
 --Gets around caching
 function getUrlSuffix()
-    return "?t=" .. math.random(1, 10000)
+    return ("?t=" .. math.random(1, 10000))
 end
